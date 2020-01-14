28528a23d9f4ad4eaf548fc568fb898236df1b79
Mohamed Samir <mohamedsamirsalem100@gmail.com>
2020-01-14 17:48:32 +0200

    Tab bar added

--- lib/pages/HomeScaffold.dart
+++ lib/pages/HomeScaffold.dart
@@ -4,12 +4,40 @@ import '../ProductManager.dart';
 class HomeScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('List View'),
-      ),
-      body: Container(
-        child: ProductManager(startingProduct: 'initial data'),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(
+                //for deleting menu button when open menu drawer
+                automaticallyImplyLeading: false,
+                title: Text('Choose'),
+              ),
+              ListTile(
+                title: Text('Manage Products'),
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: Text('List View'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                text: 'First',
+              ),
+              Tab(
+                text: 'Second',
+              ),
+            ],
+          ),
+        ),
+        body: Container(
+          child: ProductManager(startingProduct: 'initial data'),
+        ),
       ),
     );
   }
